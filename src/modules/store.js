1c306a201989a785db960c7671f74a89fac604d2
memelopez <meme30lopez@gmail.com>
2022-03-24 16:30:54 -0600

    Add store class for local storage operations

---
+++ src/modules/store.js
@@ -0,0 +1,19 @@
+// /src/modules/store.js - Store Class: Encapsulates de locacl storage
+
+export default class Store {
+  // gets tasks from storage and returns an array of objects
+  static getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    return tasks;
+  }
+
+  static setTasks(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+}
