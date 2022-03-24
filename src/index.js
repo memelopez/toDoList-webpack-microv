45356e96d101ad33e8dce237a0517165d9a7b7b3
memelopez <meme30lopez@gmail.com>
2022-03-24 17:31:09 -0600

    Remove hardcoded variables

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 // /src/index.js - entry point
 
 import './styles.css';
-import Task from './modules/task';
 import UI from './modules/ui';
 import { validateDescription } from './modules/helpfulFunctions';
-
-const task1 = new Task('wash car', 1);
-const task2 = new Task('clean room', 2);
-const task3 = new Task('mow lawn', 3);
-
-// tasks array
-const todos = [];
-todos.push(task1);
-todos.push(task2);
-todos.push(task3);
 
 // Event: on content load
 document.addEventListener('DOMContentLoaded', UI.displayTasks());
