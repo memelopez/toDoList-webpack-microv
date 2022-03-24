bdc7b16d47410b23a300ccc6a7e0b2668731edf8
memelopez <meme30lopez@gmail.com>
2022-03-24 14:36:13 -0600

    Add hardcodded todos array and eventlistener DOM content load

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 // /src/index.js - entry point
+
 import './styles.css';
+import Task from './modules/task';
+import UI from './modules/ui';
+
+const task1 = new Task('wash car', 1);
+const task2 = new Task('clean room', 2);
+const task3 = new Task('mow lawn', 3);
+
+// tasks array
+const todos = [];
+todos.push(task1);
+todos.push(task2);
+todos.push(task3);
+
+// Event: on content load
+document.addEventListener('DOMContentLoaded', UI.displayTasks(todos));
