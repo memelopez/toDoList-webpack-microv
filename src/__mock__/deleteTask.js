a9f8b029f4eb0b5f17c883134000b8d7b31b018e
memelopez <meme30lopez@gmail.com>
2022-04-06 16:36:00 -0600

    Fix linters

--- src/__mock__/deleteTask.js
+++ src/__mock__/deleteTask.js
@@ -1,36 +1,36 @@
 import { globaldocument } from '../../dynamic';
 import Store from './store';
-​
+
 const deleteTask = (index) => {
   let todos = Store.getEmptyTasks();
-​
+
   if (index <= 0 || index > todos.length) {
     return false;
   }
   todos = todos.splice(index - 1, 1);
-​
+
   const tasklist = globaldocument.getElementById('task-list');
   const taskListchildren = tasklist.children;
   const item2remove = taskListchildren[index - 1];
   item2remove.remove();
-​
+
   return Store.setTasks();
 };
-​
+
 const deleteTask2 = (index) => {
   let todos = Store.getTasksWith3Tasks();
-​
+
   if (index <= 0 || index > todos.length) {
     return false;
   }
   todos = todos.splice(index - 1, 1);
-​
+
   const tasklist = globaldocument.getElementById('task-list');
   const taskListchildren = tasklist.children;
   const item2remove = taskListchildren[index - 1];
   item2remove.remove();
-​
+
   return Store.setTasks();
 };
-​
+
 export { deleteTask, deleteTask2 };
--- src/__test__/deleteTask.test.js
+++ src/__test__/deleteTask.test.js
@@ -1,23 +1,23 @@
 // __tests__/addTask.test
 import { deleteTask, deleteTask2 } from '../__mock__/deleteTask';
 import { addNewTask, getAmountOfTaskFromDOM } from '../__mock__/addTask';
-​
+
 describe('Tests to delete a task from DOM and storage:', () => {
   test('should return false when index is less than zero', () => {
     expect(deleteTask(-1)).toBeFalsy();
   });
-​
+
   test("should return false when index is greater than length of stored ToDo's. length=0 right now", () => {
     expect(deleteTask(3)).toBeFalsy();
   });
-​
+
   test("should return true when index is from 1 to length of stored ToDo's. First we add 3 tasks and index = 2 is entered.", () => {
     addNewTask('wash car', 1);
     addNewTask('clean room', 2);
     addNewTask('mow lawn', 3);
     expect(deleteTask2(2)).toBeTruthy();
   });
-​
+
   test('should return the amount of tasks in DOM which is 2 now, because we added 3 tasks initally and deleted one.', () => {
     expect(getAmountOfTaskFromDOM()).toBe(2);
   });
