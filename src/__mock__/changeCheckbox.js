8d543519a0d61c59dc68879a31e33e4fbff2e4b3
Oluyemi Paul <oluola96@gmail.com>
2022-04-07 15:16:52 -0600

    Add changeCheckbox.js to mock folder

---
+++ src/__mock__/changeCheckbox.js
@@ -0,0 +1,36 @@
+// /src/__mock__/changeCheckbox.js - mock of the functions related to changing a checkbox
+
+import { globaldocument } from '../../dynamic';
+import Store from './store';
+
+const changeCheckbox = (index, value) => {
+  const todos = Store.getTasksWith3Tasks();
+
+  if (index > 0 && index <= todos.length) {
+    const item2change = todos[index - 1];
+    item2change.isCompleted = value;
+
+    // now we changed the newDesc in DOM
+    const tasklist = globaldocument.getElementById('task-list');
+    const li2change = tasklist.children[index - 1];
+    const checkboxToChange = li2change.children[0];
+    checkboxToChange.checked = value;
+
+    return Store.setTasks(); // Setting the tasks in the mockec store class always retunrs TRUE
+  }
+  return 'Index must be between 1 and stored tasks length.';
+};
+
+const verifyCheckboxStatus = (index, value) => {
+  const tasklist = globaldocument.getElementById('task-list');
+  const li2change = tasklist.children[index - 1];
+  const checkboxToChange = li2change.children[0];
+  const checkboxValue = checkboxToChange.checked;
+
+  if (checkboxValue === value) {
+    return true;
+  }
+  return false;
+};
+
+export { changeCheckbox, verifyCheckboxStatus };
